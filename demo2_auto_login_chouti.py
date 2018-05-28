424ae110e6b6850411ca9c268fc36a0b4a30cce1
kidellwq <kidell@163.com>
2018-05-28 23:34:06 +0800

    自动登录抽屉并进入个人中心设置页面

--- demo2_auto_login_chouti.py
+++ demo2_auto_login_chouti.py
@@ -1,13 +1,34 @@
 import requests
 
-post_diict = {
-	'phone': 15939949937,
-	'password': 111111,
-	'oneMonth': 1,
+post_dict = {
+    'phone': '8615939949937',
+    'password': '123456',
+    'oneMonth': 1,
 }
-response = requests.post(
-	url = "https://dig.chouti.com/login",
-	data = post_diict
+
+header = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/66.0.335'
+                  '9.181 Safari/537.36',
+}
+res1 = requests.get(
+    url="https://dig.chouti.com/",
+    headers=header,
+)
+cookies_dict1 = res1.cookies.get_dict()
+res2 = requests.post(
+    url="https://dig.chouti.com/login",
+    data=post_dict,
+    headers=header,
+    cookies=cookies_dict1,
+)
+cookies_dict2 = res2.cookies.get_dict()
+res3 = requests.get(
+    url="https://dig.chouti.com/profile",
+    headers=header,
+    cookies={'gpsd': cookies_dict1['gpsd']}
 )
 
-print(response.text)
+cookies_dict3 = res3.cookies.get_dict()
+print(cookies_dict1)
+print(cookies_dict2)
+print(res3.text)
